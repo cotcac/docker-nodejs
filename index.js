a97a62aea3dd6352dac0a1e09dc13db727c23ab9
Vuong <vuong@vuong.com>
2021-05-08 22:24:01 +0700

    update docker env

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  const env = {
+    PORT: process.env.PORT,
+    DB_HOST: process.env.DB_HOST,
+    DB_USER: process.env.DB_USER,
+    DB_PASS: process.env.DB_PASS
+  }
+  res.json(env)
 });
 
 app.listen(4000, function () {
